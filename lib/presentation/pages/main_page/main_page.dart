0690ad3a8652f7cd4fd36a0a3849f92ec692cd9d
masas112 <muhammad.asrori112@gmail.com>
2023-11-02 22:40:02 +0700

    36. Bottom Bar & Bottom Nav Bar Item

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,5 +1,8 @@
 import 'package:flix_id/presentation/extensions/build_context_extension.dart';
+import 'package:flix_id/presentation/misc/methods.dart';
 import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
+import 'package:flix_id/presentation/widgets/bottom_nav_bar.dart';
+import 'package:flix_id/presentation/widgets/bottom_nav_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,29 +19,43 @@ class _MainPageState extends ConsumerState<MainPage> {
   @override
   Widget build(BuildContext context) {
     ref.listen(userDataProvider, (previous, next) {
-      if(previous != null && next is AsyncData && next.value == null) {
+      if (previous != null && next is AsyncData && next.value == null) {
         ref.read(routerProvider).goNamed('login');
-      } else if(next is AsyncError) {
+      } else if (next is AsyncError) {
         context.showSnackBar(next.error.toString());
       }
     });
 
     return Scaffold(
       appBar: AppBar(title: const Text("Main Page")),
-      body: Center(
-        child: Column(
-          children: [
-            Text(ref.watch(userDataProvider).when(
-                data: (data) => data.toString(),
-                error: (error, stackTrace) => '',
-                loading: () => '')),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(userDataProvider.notifier).logout();
-              }, child: const Text("Logout"),
-            )
-          ],
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                Text(ref.watch(userDataProvider).when(
+                    data: (data) => data.toString(),
+                    error: (error, stackTrace) => '',
+                    loading: () => '')),
+                ElevatedButton(
+                  onPressed: () {
+                    ref.read(userDataProvider.notifier).logout();
+                  },
+                  child: const Text("Logout"),
+                ),
+                verticalSpace(50),
+              ],
+            ),
+          ),
+          BottomNavBar(items: const [
+            BottomNavBarItem(
+                index: 0,
+                isSelected: false,
+                title: 'Home',
+                image: 'assets/movie.png',
+                selectedImage: 'assets/movie-selected.png'),
+          ], onTap: (index) {}, selectedIndex: 0)
+        ],
       ),
     );
   }
