98edf2d696f488896286a5d4e06b946e7ceed205
masas112 <muhammad.asrori112@gmail.com>
2023-11-03 22:21:23 +0700

    41. Movie Page Bagian 2 - add poster ini upcoming

--- lib/presentation/pages/movie_page/methods/movie_list.dart
+++ lib/presentation/pages/movie_page/methods/movie_list.dart
@@ -1,5 +1,7 @@
+import 'package:flix_id/presentation/widgets/network_image_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../../domain/entities/movie.dart';
 
@@ -8,4 +10,45 @@ List<Widget> movieList({
   void Function(Movie movie)? onTap,
   required AsyncValue<List<Movie>> movies,
 }) =>
-    [];
+    [
+      Padding(
+        padding: const EdgeInsets.only(left: 24, bottom: 15),
+        child: Text(
+          title,
+          style: GoogleFonts.poppins(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 228,
+        child: movies.when(
+          data: (data) => SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: data
+                  .map(
+                    (movie) => Padding(
+                      padding: EdgeInsets.only(
+                        left: movie == data.first ? 24 : 10,
+                        right: movie == data.last ? 24 : 0,
+                      ),
+                      child: NetworkImageCard(
+                        imageUrl:
+                            'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                        fit: BoxFit.contain,
+                        onTap: () => onTap?.call(movie),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          error: (error, stackTrace) => const SizedBox(),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      )
+    ];
